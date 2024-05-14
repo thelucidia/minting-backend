231ac80ceb8eb8de803ff7319fc5d78431a008a9
Smart-Dev00214 <smart.dev00214@gmail.com>
2024-05-14 22:38:55 +0200

    nothing

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ require("dotenv").config();
 const app = express();
 app.use(cors());
 const connectDB = require("./config/db");
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3002;
 
 const userRoutes = require("./routes/userRoutes");
 
