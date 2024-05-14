03ded60940be753e6f588b9d51953e2e780c4900
Smart-Dev00214 <smart.dev00214@gmail.com>
2024-05-14 22:04:56 +0200

    fixed cors error

--- server.js
+++ server.js
@@ -7,20 +7,20 @@ require("dotenv").config();
 const app = express();
 const connectDB = require("./config/db");
 const PORT = process.env.PORT || 5000;
+app.use(cors());
 
 const userRoutes = require("./routes/userRoutes");
 
 connectDB();
 
 app.use(bodyParser.json());
-app.use(cors());
 
 app.use("/api/user", userRoutes);
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+  res.send("Hello World!");
 });
 
 app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`)
+  console.log(`Server started on port ${PORT}`);
 });
