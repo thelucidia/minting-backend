504e069c4971fb7b87629202beb00d8f655e86a6
DESKTOP-SHOO1R4\BigBoss <promotionlee.bs@gmail.com>
2024-05-22 01:14:06 -0700

    add the backend service for email auth, login community

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,20 @@
+require('dotenv').config();
+const nodemailer = require("nodemailer");
+const Twitter = require('twitter');
+const TelegramBot = require("node-telegram-bot-api");
+const { Client, GatewayIntentBits } = require("discord.js");
 const userService = require("../services/userService");
 
-const nodemailer = require("nodemailer");
+const twitterClient = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+  bearer_token: process.env.TWITTER_BEARER_TOKEN,
+});
+const bot = new TelegramBot(process.env.TEL_BOT_TOKEN, { polling: true });
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+client.login(process.env.DIS_BOT_TOKEN);
 
 // In-memory storage for simplicity
 let verificationCodes = {};
@@ -42,10 +56,11 @@ const sendCode = async (req, res) => {
 
 const verifyCode = (req, res) => {
   console.log("req.body: ", req.body.email, req.body.code);
-  const { email, code } = req.body;
+  const { email, code, wallet } = req.body;
   const validCode = verificationCodes[email];
   console.log("ValidCode: ", code);
   if (parseInt(code) === validCode) {
+    userService.create({ email, wallet })
     delete verificationCodes[email]; // Remove code after verification
     res.json({ msg: "Verification successful" });
   } else {
@@ -53,7 +68,68 @@ const verifyCode = (req, res) => {
   }
 };
 
+const checkFollowingTwitter = async (req, res) => {
+  const { username } = req.body;
+
+  if (!username) {
+    return res.status(400).json({ error: 'username is required' });
+  }
+
+  try {
+    const followingList = await twitterClient.get('friends/list', {
+      screen_name: 'lucidia_io'
+    });
+    console.log(followingList);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+const checkMembershipForTelegram = async (req, res) => {
+  const { username } = req.body;
+
+  try {
+    const admins = await bot.getChatAdministrators(process.env.TEL_GROUP_ID);
+    console.log(admins);
+    const adminMember = admins.find(admin => admin.user.username === username);
+    if (adminMember) {
+      return res.json({ membership: "Admin" });
+    }
+
+    const memberStatus = await bot.getChatMember(groupId, username);
+    if (['member', 'administrator', 'creator'].includes(memberStatus.status))
+      return res.json({ membership: memberStatus.status });
+
+    res.json({ membership: 'none' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred while checking the membership of Telegram' });
+  }
+}
+
+const checkMembershipForDiscord = async (req, res) => {
+  const { username } = req.body;
+
+  try {
+    const guild = await client.guilds.cache.get(process.env.DIS_SERVER_ID);
+    const members = await guild.members.search({ query: username });
+    const member = members.find(member => member.user.username === username);
+
+    if (member) {
+      res.json({ isMember: true });
+    } else {
+      res.json({ isMember: false });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: 'An error occurred while checking the membership of Discord' });
+  }
+}
+
 module.exports = {
   sendCode,
   verifyCode,
+  checkFollowingTwitter,
+  checkMembershipForTelegram,
+  checkMembershipForDiscord
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,5 +7,8 @@ const userController = require("../controllers/userController");
 
 router.post("/send-code", userController.sendCode);
 router.post("/verify-code", userController.verifyCode);
+router.post("/check-following", userController.checkFollowingTwitter);
+router.post("/check-username-telegram", userController.checkMembershipForTelegram);
+router.post("/check-username-discord", userController.checkMembershipForDiscord);
 
 module.exports = router;
