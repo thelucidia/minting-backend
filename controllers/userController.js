11e3bbd75ac046a2e46575223a7e0f2ea3466a20
DESKTOP-SHOO1R4\BigBoss <promotionlee.bs@gmail.com>
2024-05-22 09:04:30 -0700

    update the email verification

--- controllers/userController.js
+++ controllers/userController.js
@@ -30,41 +30,38 @@ const transporter = nodemailer.createTransport({
 
 // Endpoint to generate and send a code
 const sendCode = async (req, res) => {
-  console.log("email: ", req.body.email);
   const { email } = req.body;
   const code = Math.floor(1000 + Math.random() * 9000);
   verificationCodes[email] = code; // Store code
-  console.log("pass: ", email);
   try {
-    console.log("1: ", transporter.sendMail);
-
     const mailOptions = {
-      from: "smartdev0214@gmail.com",
+      from: "auth@lucidia.io",
       to: email,
       subject: "Your Verification Code",
       text: `Your verification code is : ${code}`,
     };
 
     await transporter.sendMail(mailOptions);
-    console.log("2:");
     res.json({ email });
   } catch (error) {
-    console.log("3: ", error);
+    console.log(error);
     res.status(500).json({ msg: "Failed to send email" });
   }
 };
 
 const verifyCode = (req, res) => {
-  console.log("req.body: ", req.body.email, req.body.code);
   const { email, code, wallet } = req.body;
   const validCode = verificationCodes[email];
-  console.log("ValidCode: ", code);
-  if (parseInt(code) === validCode) {
-    userService.create({ email, wallet })
-    delete verificationCodes[email]; // Remove code after verification
-    res.json({ msg: "Verification successful" });
-  } else {
-    res.json({ msg: "Verification failed" });
+  try {
+    if (parseInt(code) === validCode) {
+      userService.create({ email, wallet })
+      delete verificationCodes[email]; // Remove code after verification
+      res.json({ msg: "Verification successful" });
+    } else {
+      res.json({ msg: "Verification failed" });
+    }
+  } catch (error) {
+    console.log(error);
   }
 };
 
